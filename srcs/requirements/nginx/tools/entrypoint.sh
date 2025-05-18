e94b722cc7553786756b92dec426533a56885ca8
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-05-18 20:00:15 +0000

    feat(nginx): nome do arquivo errado

--- srcs/requirements/nginx/tools/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitui variáveis no template e gera o nginx.conf final
-envsubst '$CERT_PATH $KEY_PATH' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Inicia o NGINX
-exec nginx -g 'daemon off;'
